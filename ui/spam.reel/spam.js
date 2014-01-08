bf1a374629375debb64e3be0bec8b68c5993600a
Mike Czepiel <mike@czepiel.net>
2014-01-08 11:59:35 -0800

    Add component spam

---
+++ ui/spam.reel/spam.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/spam.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Spam
+ * @extends Component
+ */
+exports.Spam = Component.specialize(/** @lends Spam# */ {
+    constructor: {
+        value: function Spam() {
+            this.super();
+        }
+    }
+});
