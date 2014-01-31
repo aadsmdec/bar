f7d5be8df8addb2f3c3fa05f205f82660eca6187
Afonso <noreply>
2014-01-31 02:23:03 +0000

    Update component /ui/big-dom.reel/

---
+++ ui/big-dom.reel/big-dom.js
@@ -0,0 +1,17 @@
+/**
+ * @module ./big-dom.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class BigDom
+ * @extends Component
+ */
+exports.BigDom = Component.specialize(/** @lends BigDom# */ {
+    constructor: {
+        value: function BigDom() {
+            this.super();
+        }
+    }
+});
---
+++ ui/big-dom2.reel/big-dom2.js
@@ -0,0 +1,17 @@
+/**
+ * @module ./big-dom2.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class BigDom2
+ * @extends Component
+ */
+exports.BigDom2 = Component.specialize(/** @lends BigDom2# */ {
+    constructor: {
+        value: function BigDom2() {
+            this.super();
+        }
+    }
+});
